662d71a2d371f926d7a5d43c1cb2b3658e411eee
AharonFixler <ariandleah@gmail.com>
2022-06-30 22:07:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,40 +37,40 @@ def index():
                     user=os.environ['ksbqirnakrdgji'],
                     password=os.environ['b2a4e8c890bd2da698980e5e14e4e73c43139f14c114d4fa8961866c1fe3cdc8'])
             # Open a cursor to perform database operations
-                cur = conn.cursor()
+            cur = conn.cursor()
 
-                # Execute a command: this creates a new table
-                cur.execute('DROP TABLE IF EXISTS books;')
-                cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                                 'title varchar (150) NOT NULL,'
-                                                 'author varchar (50) NOT NULL,'
-                                                 'pages_num integer NOT NULL,'
-                                                 'review text,'
-                                                 'date_added date DEFAULT CURRENT_TIMESTAMP);'
-                                                 )
+            # Execute a command: this creates a new table
+            cur.execute('DROP TABLE IF EXISTS books;')
+            cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+                                             'title varchar (150) NOT NULL,'
+                                             'author varchar (50) NOT NULL,'
+                                             'pages_num integer NOT NULL,'
+                                             'review text,'
+                                             'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                             )
 
-                # Insert data into the table
+            # Insert data into the table
 
-                cur.execute('INSERT INTO books (title, author, pages_num, review)'
-                            'VALUES (%s, %s, %s, %s)',
-                            ('A Tale of Two Cities',
-                             'Charles Dickens',
-                             489,
-                             'A great classic!')
-                            )
+            cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                        'VALUES (%s, %s, %s, %s)',
+                        ('A Tale of Two Cities',
+                         'Charles Dickens',
+                         489,
+                         'A great classic!')
+                        )
 
 
-                cur.execute('INSERT INTO books (title, author, pages_num, review)'
-                            'VALUES (%s, %s, %s, %s)',
-                            ('Anna Karenina',
-                             'Leo Tolstoy',
-                             864,
-                             'Another great classic!')
-                            )
+            cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                        'VALUES (%s, %s, %s, %s)',
+                        ('Anna Karenina',
+                         'Leo Tolstoy',
+                         864,
+                         'Another great classic!')
+                        )
 
-                conn.commit()
+            conn.commit()
 
-                cur.close()
+            cur.close()
                 conn.close()
         else:
             pass # unknown
